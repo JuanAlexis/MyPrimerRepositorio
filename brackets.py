ed8bb572ff0167d64bfbfcd8a0293069475a9f40
JuanAlexis <56707058+JuanAlexis@users.noreply.github.com>
2020-06-04 13:21:54 -0500

    Add files via upload

---
+++ brackets.py
@@ -0,0 +1,7 @@
+Mylist = [10,20,30,40,50,60,70,80,90]
+# Te da la cantidad de elementos de una lista
+print (len(Mylist))
+
+#Impresion de todos los valores de una lista
+for i in Mylist:
+    print (i)
